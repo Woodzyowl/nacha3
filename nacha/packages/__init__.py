9ce311da0661341203d5ea5cfc669e4b40625332
Rajendra <rajendra.g@amazatic.com>
2019-07-04 10:07:32 +0530

    fixed issue for python 3

--- nacha/packages/__init__.py
+++ nacha/packages/__init__.py
@@ -1,9 +1,8 @@
 from __future__ import absolute_import
-
 import sys
 
 if sys.version_info[0] < 3:
-    from .nacha2_init import *
+    from .bryl import *
 else:
-    from .nacha3_init import *
+    from .bryl3 import *
 
